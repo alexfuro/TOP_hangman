e47a9d53826e18068d61747a44623c2e012fcf00
AlexFuro <afurones43@gmail.com>
2018-05-11 11:36:18 -0400

    Fix input handling for capital letters

--- player.rb
+++ player.rb
@@ -8,6 +8,7 @@ class Player
   end
   def user_guess(guess)
     return if guess.nil?
+    guess = guess.downcase
     if guess.length == 1
       if !@guesses.include?(guess)
         @guesses.push(guess)
